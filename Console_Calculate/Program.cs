21d24004175ca35e8ad652678d68384352ca117d
Hidenman <elderlutique@gmail.com>
2023-01-08 19:51:00 +0200

    Add project files.

---
+++ Console_Calculate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Calculate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double f, s;
+            char c;
+            Console.WriteLine("\t******Calculate******");
+            Console.Write($"enter first Operand: ");
+            f = double.Parse(Console.ReadLine());
+            Console.Write($"enter second Operand: ");
+            s = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("What do you want?");
+            c = char.Parse(Console.ReadLine());
+
+            switch (c)
+            {
+                case '+':
+                    Console.WriteLine(f+s);
+                    break;
+                case '-':
+                    Console.WriteLine(f-s);
+                    break;
+                case '/':
+                    Console.WriteLine(f/s);
+                    break;
+                case '*':
+                    Console.WriteLine(f*s);
+                    break;
+                case '%':
+                    Console.WriteLine(f%s);
+                    break;
+            }
+        }
+
+    }
+}
